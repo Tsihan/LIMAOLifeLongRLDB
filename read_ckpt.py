83bc30898dce9f9f0c451c9e9276451c33a94749
“Tsihan” <2453939195@qq.com>
2024-03-20 00:06:05 +0000

    no read file py

--- read_ckpt.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-ckpt_path = '/home/qihan/balsa_project/balsaLifeLongRLDB/tensorboard_logs_/0_jg6x6uzs/checkpoints/epoch=9.ckpt'
-checkpoint = torch.load(ckpt_path)
-print(ckpt_path)
-for name, param in checkpoint.items():
-    print("Tensor name:", name)
-    print("Value:", param)
-
