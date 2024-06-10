77a196c7edd05c4183449ffed33edca6d2fefe9c
qihanzhang <2453939195@qq.com>
2024-06-10 06:40:14 +0000

    add SO queries

---
+++ queries/sample_queries_so/q1-002.sql
@@ -0,0 +1,10 @@
+SELECT COUNT(*)
+FROM tag AS t1, site AS s1, question AS q1, tag_question AS tq1
+WHERE
+s1.site_name = 'serverfault' AND
+t1.name = 'psql' AND
+t1.site_id = s1.site_id AND
+q1.site_id = s1.site_id AND
+tq1.site_id = s1.site_id AND
+tq1.question_id = q1.id AND
+tq1.tag_id = t1.id;
