ef588bf9035bf9d90da95e0d1bca362190851ac1
Tsihan <74820568+Tsihan@users.noreply.github.com>
2024-06-20 00:43:26 -0700

    m 3 4

--- queries/imdb_assorted_3/17a_job.sql
+++ queries/imdb_assorted_3/17a_job.sql
@@ -1,50 +1,22 @@
-SELECT COUNT(*)
-FROM title as t,
-movie_info as mi1,
-kind_type as kt,
-info_type as it1,
-info_type as it3,
-info_type as it4,
-movie_info_idx as mi_idx1,
-movie_info_idx as mi_idx2,
-movie_keyword as mk,
-keyword as k,
-aka_name as an,
-name as n,
-info_type as it5,
-person_info as pi1,
-cast_info as ci,
-role_type as rt
-WHERE
-t.id = mi1.movie_id
-AND t.id = ci.movie_id
-AND t.id = mi_idx1.movie_id
-AND t.id = mi_idx2.movie_id
-AND t.id = mk.movie_id
-AND mk.keyword_id = k.id
-AND mi1.info_type_id = it1.id
-AND mi_idx1.info_type_id = it3.id
-AND mi_idx2.info_type_id = it4.id
-AND t.kind_id = kt.id
-AND (kt.kind IN ('tv movie','video movie'))
-AND (t.production_year <= 2015)
-AND (t.production_year >= 1925)
-AND (mi1.info IN ('Italy','Japan','Nigeria','Spain','West Germany'))
-AND (it1.id IN ('8'))
-AND it3.id = '100'
-AND it4.id = '101'
-AND (mi_idx2.info ~ '^(?:[1-9]\d*|0)?(?:\.\d+)?$' AND mi_idx2.info::float <= 8.0)
-AND (mi_idx2.info ~ '^(?:[1-9]\d*|0)?(?:\.\d+)?$' AND 0.0 <= mi_idx2.info::float)
-AND (mi_idx1.info ~ '^(?:[1-9]\d*|0)?(?:\.\d+)?$' AND 1000.0 <= mi_idx1.info::float)
-AND (mi_idx1.info ~ '^(?:[1-9]\d*|0)?(?:\.\d+)?$' AND mi_idx1.info::float <= 10000.0)
-AND n.id = ci.person_id
-AND ci.person_id = pi1.person_id
-AND it5.id = pi1.info_type_id
-AND n.id = pi1.person_id
-AND n.id = an.person_id
-AND rt.id = ci.role_id
-AND (n.gender in ('f'))
-AND (n.name_pcode_nf in ('C6235','E4213') OR n.name_pcode_nf IS NULL)
-AND (ci.note IS NULL)
-AND (rt.role in ('actress'))
-AND (it5.id in ('25'))
+SELECT MIN(n.name) AS member_in_charnamed_american_movie,
+       MIN(n.name) AS a1
+FROM cast_info AS ci,
+     company_name AS cn,
+     keyword AS k,
+     movie_companies AS mc,
+     movie_keyword AS mk,
+     name AS n,
+     title AS t
+WHERE cn.country_code ='[us]'
+  AND k.keyword ='character-name-in-title'
+  AND n.name LIKE 'B%'
+  AND n.id = ci.person_id
+  AND ci.movie_id = t.id
+  AND t.id = mk.movie_id
+  AND mk.keyword_id = k.id
+  AND t.id = mc.movie_id
+  AND mc.company_id = cn.id
+  AND ci.movie_id = mc.movie_id
+  AND ci.movie_id = mk.movie_id
+  AND mc.movie_id = mk.movie_id;
+
--- queries/imdb_assorted_3/6b_job.sql
+++ queries/imdb_assorted_3/6b_job.sql
@@ -1,1 +1,24 @@
-SELECT MIN(an.name) AS cool_actor_pseudonym, MIN(t.title) AS series_named_after_char FROM aka_name AS an, cast_info AS ci, company_name AS cn, keyword AS k, movie_companies AS mc, movie_keyword AS mk, name AS n, title AS t WHERE cn.country_code ='[uk]' AND k.keyword ='character-name-in-title' AND an.person_id = n.id AND n.id = ci.person_id AND ci.movie_id = t.id AND t.id = mk.movie_id AND mk.keyword_id = k.id AND t.id = mc.movie_id AND mc.company_id = cn.id AND an.person_id = ci.person_id AND ci.movie_id = mc.movie_id AND ci.movie_id = mk.movie_id AND mc.movie_id = mk.movie_id;
+SELECT MIN(k.keyword) AS movie_keyword,
+       MIN(n.name) AS actor_name,
+       MIN(t.title) AS hero_movie
+FROM cast_info AS ci,
+     keyword AS k,
+     movie_keyword AS mk,
+     name AS n,
+     title AS t
+WHERE k.keyword IN ('superhero',
+                    'sequel',
+                    'second-part',
+                    'marvel-comics',
+                    'based-on-comic',
+                    'tv-special',
+                    'fight',
+                    'violence')
+  AND n.name LIKE '%Downey%Robert%'
+  AND t.production_year > 2014
+  AND k.id = mk.keyword_id
+  AND t.id = mk.movie_id
+  AND t.id = ci.movie_id
+  AND ci.movie_id = mk.movie_id
+  AND n.id = ci.person_id;
+
