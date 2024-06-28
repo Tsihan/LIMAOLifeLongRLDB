9a0899f4f12e6ab1d2d3db3649b13175ba30b2df
Tsihan <74820568+Tsihan@users.noreply.github.com>
2024-06-27 22:54:16 -0700

    debug

--- sim.py
+++ sim.py
@@ -515,11 +515,11 @@ class Sim(object):
             "Search until how many complete plans?",
         )
         # Workload.
-        p.Define(
-            "workload",
-            envs.IMDB_assorted_small.Params(),
-            "Params of the Workload, i.e., a set of queries.",
-        )
+        # p.Define(
+        #     "workload",
+        #     envs.IMDB_assorted_small.Params(),
+        #     "Params of the Workload, i.e., a set of queries.",
+        # )
         
         # p.Define(
         #     "workload",
@@ -540,11 +540,11 @@ class Sim(object):
         # )
 
 
-        # p.Define(
-        #     "workload",
-        #     envs.IMDB_assorted_3.Params(),
-        #     "Params of the Workload, i.e., a set of queries.",
-        # )
+        p.Define(
+            "workload",
+            envs.IMDB_assorted_3.Params(),
+            "Params of the Workload, i.e., a set of queries.",
+        )
 
         # p.Define(
         #     "workload",
@@ -811,11 +811,11 @@ class Sim(object):
         p = self.params
         hash_key = Sim.HashOfSimData(p)
         # return 'data/JOB_changed/sim-data-{}.pkl'.format(hash_key)
-        return "data/IMDB_assorted_small/sim-data-{}.pkl".format(hash_key)
+        #return "data/IMDB_assorted_small/sim-data-{}.pkl".format(hash_key)
         #return "data/IMDB_assorted_small_2/sim-data-{}.pkl".format(hash_key)
         #return "data/IMDB_assorted/sim-data-{}.pkl".format(hash_key)
         #return "data/IMDB_assorted_2/sim-data-{}.pkl".format(hash_key)
-        # return "data/IMDB_assorted_3/sim-data-{}.pkl".format(hash_key)
+        return "data/IMDB_assorted_3/sim-data-{}.pkl".format(hash_key)
         # return "data/IMDB_assorted_4/sim-data-{}.pkl".format(hash_key)
         # return "data/TPCH10_assorted/sim-data-{}.pkl".format(hash_key)
         # return "data/TPCH10_assorted_2/sim-data-{}.pkl".format(hash_key)
@@ -859,11 +859,11 @@ class Sim(object):
     def _FeaturizedDataPath(self):
         p = self.params
         hash_key = Sim.HashOfFeaturizedData(p)
-        return "data/IMDB_assorted_small/sim-featurized-{}.pkl".format(hash_key)
+        #return "data/IMDB_assorted_small/sim-featurized-{}.pkl".format(hash_key)
         # return "data/IMDB_assorted_small_2/sim-featurized-{}.pkl".format(hash_key)
         #return "data/IMDB_assorted/sim-featurized-{}.pkl".format(hash_key)
         #return "data/IMDB_assorted_2/sim-featurized-{}.pkl".format(hash_key)
-        # return "data/IMDB_assorted_3/sim-featurized-{}.pkl".format(hash_key)
+        return "data/IMDB_assorted_3/sim-featurized-{}.pkl".format(hash_key)
         # return "data/IMDB_assorted_4/sim-featurized-{}.pkl".format(hash_key)
         # return "data/TPCH10_assorted/sim-featurized-{}.pkl".format(hash_key)
         # return "data/TPCH10_assorted_2/sim-featurized-{}.pkl".format(hash_key)
