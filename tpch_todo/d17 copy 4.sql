b6438435a0a61cde080db7ef29e5c98ced30ce91
tsihan <2453939195@qq.com>
2025-03-27 00:29:37 -0500

    add

---
+++ tpch_todo/d17 copy 4.sql
@@ -0,0 +1,12 @@
+select
+	sum(l_extendedprice) / 7.0 as avg_yearly
+from
+	lineitem,
+	part,
+        (select l_partkey as agg_partkey, 0.2 * avg(l_quantity) as avg_quantity from lineitem group by l_partkey) part_agg
+where
+	p_partkey = l_partkey
+        and agg_partkey = l_partkey
+	and p_brand = 'Brand#15'
+	and p_container = 'MED DRUM'
+	and l_quantity < avg_quantity;
