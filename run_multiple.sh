f6e5ab7a1afdbc4e0ada679a1e26c473e55c9bc5
tsihan <2453939195@qq.com>
2025-03-28 01:53:21 -0500

    add

--- run_multiple.sh
+++ run_multiple.sh
@@ -2,66 +2,76 @@
 source /mydata/anaconda3/bin/activate balsa
 
 ############################################
+cd /mydata/LIMAOLifeLongRLDB
 rm ./logfile
 rm /mydata/LIMAOLifeLongRLDB/bao_server/bao_default_model
 rm /mydata/LIMAOLifeLongRLDB/bao_server/bao_previous_model
 rm /mydata/LIMAOLifeLongRLDB/bao_server/bao.db
 pg_ctl -D /mydata/databases restart -l logfile
-python /mydata/LIMAOLifeLongRLDB/bao_server/main.py > /mydata/arm_selection_1.log 2>&1 &
+cd /mydata/LIMAOLifeLongRLDB/bao_server
+python ./main.py > /mydata/arm_selection_1.log 2>&1 &
 MAIN_PID=$!
 sleep 10 
 python /mydata/LIMAOLifeLongRLDB/run_queries_assorted.py --seed 10 > /mydata/bao_assorted_1.log 2>&1
 kill $MAIN_PID
 wait $MAIN_PID 2>/dev/null
 ############################################
+cd /mydata/LIMAOLifeLongRLDB
 rm ./logfile
 rm /mydata/LIMAOLifeLongRLDB/bao_server/bao_default_model
 rm /mydata/LIMAOLifeLongRLDB/bao_server/bao_previous_model
 rm /mydata/LIMAOLifeLongRLDB/bao_server/bao.db
 pg_ctl -D /mydata/databases restart -l logfile
-python /mydata/LIMAOLifeLongRLDB/bao_server/main.py > /mydata/arm_selection_2.log 2>&1 &
+cd /mydata/LIMAOLifeLongRLDB/bao_server
+python ./main.py > /mydata/arm_selection_2.log 2>&1 &
 MAIN_PID=$!
 sleep 10
 python /mydata/LIMAOLifeLongRLDB/run_queries_assorted.py --seed 20 > /mydata/bao_assorted_2.log 2>&1
 kill $MAIN_PID
 wait $MAIN_PID 2>/dev/null
 ############################################
-# Clean up and restart for the next session
+cd /mydata/LIMAOLifeLongRLDB
 rm ./logfile
 rm /mydata/LIMAOLifeLongRLDB/bao_server/bao_default_model
 rm /mydata/LIMAOLifeLongRLDB/bao_server/bao_previous_model
 rm /mydata/LIMAOLifeLongRLDB/bao_server/bao.db
 pg_ctl -D /mydata/databases restart -l logfile
-python /mydata/LIMAOLifeLongRLDB/bao_server/main.py > /mydata/arm_selection_3.log 2>&1 &
+cd /mydata/LIMAOLifeLongRLDB/bao_server
+python ./main.py > /mydata/arm_selection_3.log 2>&1 &
 MAIN_PID=$!
 sleep 10  
 python /mydata/LIMAOLifeLongRLDB/run_queries_assorted.py --seed 30 > /mydata/bao_assorted_3.log 2>&1
 kill $MAIN_PID
 wait $MAIN_PID 2>/dev/null
 ############################################
+cd /mydata/LIMAOLifeLongRLDB
 rm ./logfile
 rm /mydata/LIMAOLifeLongRLDB/bao_server/bao_default_model
 rm /mydata/LIMAOLifeLongRLDB/bao_server/bao_previous_model
 rm /mydata/LIMAOLifeLongRLDB/bao_server/bao.db
 pg_ctl -D /mydata/databases restart -l logfile
-python /mydata/LIMAOLifeLongRLDB/bao_server/main.py > /mydata/arm_selection_4.log 2>&1 &
+cd /mydata/LIMAOLifeLongRLDB/bao_server
+python ./main.py > /mydata/arm_selection_4.log 2>&1 &
 MAIN_PID=$!
 sleep 10
 python /mydata/LIMAOLifeLongRLDB/run_queries_assorted.py --seed 40 > /mydata/bao_assorted_4.log 2>&1
 kill $MAIN_PID
 wait $MAIN_PID 2>/dev/null
 ############################################
+cd /mydata/LIMAOLifeLongRLDB
 rm ./logfile
 rm /mydata/LIMAOLifeLongRLDB/bao_server/bao_default_model
 rm /mydata/LIMAOLifeLongRLDB/bao_server/bao_previous_model
 rm /mydata/LIMAOLifeLongRLDB/bao_server/bao.db
 pg_ctl -D /mydata/databases restart -l logfile
-python /mydata/LIMAOLifeLongRLDB/bao_server/main.py > /mydata/arm_selection_5.log 2>&1 &
+cd /mydata/LIMAOLifeLongRLDB/bao_server
+python ./main.py > /mydata/arm_selection_5.log 2>&1 &
 MAIN_PID=$!
 sleep 10  
 python /mydata/LIMAOLifeLongRLDB/run_queries_assorted.py --seed 50 > /mydata/bao_assorted_5.log 2>&1
 kill $MAIN_PID
 wait $MAIN_PID 2>/dev/null
+cd /mydata/LIMAOLifeLongRLDB
 rm ./logfile
 rm /mydata/LIMAOLifeLongRLDB/bao_server/bao_default_model
 rm /mydata/LIMAOLifeLongRLDB/bao_server/bao_previous_model
